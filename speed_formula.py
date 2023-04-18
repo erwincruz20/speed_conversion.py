9ca2aa95376203dc7767515ea3e90b790bb08eac
erwincruz20 <125974034+erwincruz20@users.noreply.github.com>
2023-04-18 16:50:51 +0300

    Create  speed_formula.py

---
+++ speed_formula.py
@@ -0,0 +1,6 @@
+distance = float(input("Введите расстояние в метрах: "))
+time = float(input("Введите время в секундах: "))
+
+speed = distance / time
+
+print("Скорость движения:", speed, "метров в секунду.")
